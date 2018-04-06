291f2a905c3ae3f5ed7c4600d8d6174a8f8a0c14
kaiserkumars <96deepakkumar@gmail.com>
2018-04-06 11:55:54 +0530

    added google maps

--- geoapp/views.py
+++ geoapp/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 import requests
 
 def home(request):
-    response = requests.get('https://freegeoip.net/json')
+    ip_address = request.META.get('HTTP_X_FORWARDED_FOR','')
+    response = requests.get('https://freegeoip.net/json/%s' % ip_address)
     geodata = response.json()
     context = {
         'ip':geodata['ip'],
-        'country':geodata['country_name']
+        'country':geodata['country_name'],
+        'latitude':geodata['latitude'],
+        'longitude':geodata['longitude'],
+        'api_key':'AIzaSyBLVOaZ9yIyLhyH_qHqfrNMRkblN-QPYv4'
     }
     return render(request,'geoapp/home.html',context)
 # Create your views here.
