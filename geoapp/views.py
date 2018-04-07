d5671212952305119d03f467b6df675a56f087f5
kaiserkumars <96deepakkumar@gmail.com>
2018-04-07 10:48:40 +0530

    caching session

--- geoapp/views.py
+++ geoapp/views.py
@@ -2,15 +2,21 @@ from django.shortcuts import render
 import requests
 
 def home(request):
-    ip_address = request.META.get('HTTP_X_FORWARDED_FOR','')
-    response = requests.get('https://freegeoip.net/json/%s' % ip_address)
-    geodata = response.json()
+    is_cached = ('geodata' in request.session)   #use of request.session to cache results
+    if not is_cached:
+        ip_address = request.META.get('HTTP_X_FORWARDED_FOR','')
+        response = requests.get('https://freegeoip.net/json/%s' % ip_address)
+        request.session['geodata'] = response.json() #use of request.session to cache results
+        # geodata = response.json()
+    # print(geodata['latitude'])
+    geodata=request.session['geodata']
     context = {
         'ip':geodata['ip'],
         'country':geodata['country_name'],
         'latitude':geodata['latitude'],
         'longitude':geodata['longitude'],
-        'api_key':'AIzaSyBLVOaZ9yIyLhyH_qHqfrNMRkblN-QPYv4'
+        'api_key':'AIzaSyBLVOaZ9yIyLhyH_qHqfrNMRkblN-QPYv4',
+        'is_cached':is_cached
     }
     return render(request,'geoapp/home.html',context)
 # Create your views here.
--- restGeoLoc/settings.py
+++ restGeoLoc/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'restGeoLoc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'geoapp',
+        'USER':'root',
+        'PASSWORD':'toor',
+        'HOST':'localhost',
+        'PORT':''
     }
 }
 
